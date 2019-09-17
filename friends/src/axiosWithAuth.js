64d6e60377850a6da472a017a5a586dba562b438
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-17 12:52:21 -0700

    saving

---
+++ friends/src/axiosWithAuth.js
@@ -0,0 +1,20 @@
+// From Guided Project
+
+import axios from 'axios';
+
+// Build a module that "creates" a new "instance" of the axios object
+// - baseURL
+// - headers object -> authorization header with the token
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    baseURL: 'http://localhost:5000/api/friends',
+    headers: {
+      Authorization: token
+    }
+  });
+};
+ 
+// Re-Write
